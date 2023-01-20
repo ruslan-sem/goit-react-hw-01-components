d610a2cf8b2b7117a3a2f292bf83602509670530
Ruslan Samoylenko <ruslan.sem@gmail.com>
2023-01-20 17:16:24 +0200

    FriendList

---
+++ src/components/FriendList/FriendList.js
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+import css from './FriendList.module.css';
+import { FriendListItem } from 'components/FriendListItem/FriendListItem';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className={css.friendList}>
+      {friends.map(friend => (
+        <FriendListItem key={friend.id} friend={friend} />
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    }).isRequired
+  ).isRequired,
+};
---
+++ src/components/FriendListItem/FriendListItem.js
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+import css from './FriendListItem.module.css';
+
+export const FriendListItem = ({ friend: { id, avatar, name, isOnline } }) => {
+  return (
+    <li className={css.item}>
+      <span className={isOnline ? css.statusOnline : css.statusOffline}></span>
+      <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+      <p className={css.name}>{name}</p>
+    </li>
+  );
+};
+
+FriendListItem.propTypes = {
+  friend: PropTypes.shape({
+    avatar: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    isOnline: PropTypes.bool.isRequired,
+  }).isRequired,
+};
