3ab5d7e1a8415d689b9fca92979594e402ba0389
Ruslan Samoylenko <ruslan.sem@gmail.com>
2023-01-20 15:48:19 +0200

    Statstics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,8 @@
 import { Profile } from './Profile/Profile'
+import { Statistics } from './Statistics/Statistics'
+
 import user from '../user.json'
+import data from '../data.json'
 
 export const App = () => {
   return (
@@ -7,6 +10,8 @@ export const App = () => {
       style={{
         height: '100vh',
         display: 'flex',
+        gap: '10px',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
@@ -15,6 +20,7 @@ export const App = () => {
     >
       {/* React homework template */}
       <Profile username={user.username} tag={user.tag} location={user.location} avatar={user.avatar} stats={user.stats} />
+      <Statistics title="Upload stats" stats={data} />
     </div>
     // <>
     //   <Profile username={user.username} tag={user.tag} location={user.location} avatar={user.avatar} stats={user.stats} />
